6a96fdece8d398ba959e7807caa24a8ad0709b9a
jrvvv <JorikJorik2000@gmail.com>
2023-11-10 03:19:59 +0300

    sleep works

--- user/sleep.c
+++ user/sleep.c
@@ -4,17 +4,21 @@
 
 int main(int argc, char *argv[])
 {
-  int retcode = 0;
   int time;
+  int retcode = 0;
 
-  if (argc < 0 || argc > 1) {
-    fprintf(2, "Error: Number of args is %d, 1 args only is available");
-    retcode = 1;
+  if (argc > 2) {
+    fprintf(2, "Error: Number of args is %d, 1 args only is available", argc - 1);
+    exit(1);
+  }
+  
+  if (argc <= 1) {
+    fprintf(2, "Sleep usage: sleep [time]");
   } else {
     time = atoi(argv[1]);
 
     if (time) {
-      sleep(time);
+      retcode = sleep(time);
     } else {
       fprintf(2, "Error: arg should be a number");
       retcode = 1;
