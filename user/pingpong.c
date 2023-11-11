1df71e3aa839066f9bb5beb455ce3396726cf60f
jrvvv <JorikJorik2000@gmail.com>
2023-11-11 17:00:45 +0300

    raw pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
+#include "kernel/stat.h"
+
+// int ping();
+// int pong();
+
+int main(int argc, char** argv) {
+  int ping_fd[2];
+  int pong_fd[2];
+
+  exit(0);
+}
